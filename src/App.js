f9a1897efd04ca2ef527ece0bbbe9f41204ae855
john calentine <112145061+calentine@users.noreply.github.com>
2024-08-08 01:29:27 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Wheel_Icon from './components/icons/Wheel_Icon';
+import Ai_Assist_Icon from './components/icons/Ai_Assist_Icon'
+import Android_Icon from './components/icons/Android_Icon';
+import FM_Icon from './components/icons/FM_Icon';
+import Apps_Icon from './components/icons/Apps_Icon';
+import Nav_Icon from './components/icons/Nav_Icon';
+import Trip_Comp_Icon from './components/icons/Trip_Comp_Icon';
+import Bluetooth_Icon from './components/icons/Bluetooth_Icon';
+import Phone_Icon from './components/icons/Phone_Icon'
+
+
+const iconSize = 20;
 
 function App() {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Infotainment</h1>
+      <div className="dashboard_container">
+        <div className="container">
+          <div className={`menu ${menuActive ? 'active' : ''}`}>
+            <div className="toggle" onClick={toggleMenu}>
+            <Wheel_Icon />
+            </div>
+            <li style={{ '--i': 0, backgroundImage: 'linear-gradient(orange 0%, #34335D 52%, #34335D 92%, orange 100%)' }}>
+              <a href="#"><Nav_Icon /></a>
+            </li>
+            <li style={{ '--i': 1, backgroundImage: 'linear-gradient(green 0%, #34335D 52%, #34335D 92%, green 100%)' }}>
+              <a href="#"><Phone_Icon/></a>
+            </li>
+            <li style={{ '--i': 2 , backgroundImage: 'linear-gradient(blue 0%, #34335D 52%, #34335D 92%, blue 100%)'}}>
+              <a href="#"><Android_Icon /></a>
+            </li>
+            <li style={{ '--i': 3, backgroundImage: 'linear-gradient(red 0%, #34335D 52%, #34335D 92%, red 100%)' }}>
+              <a href="#"><FM_Icon /></a>
+            </li>
+            <li style={{ '--i': 4 , backgroundImage: 'linear-gradient(orange 0%, #34335D 8%, #34335D 52%, orange 100%)'}}>
+              <a href="#"><Apps_Icon /></a>
+            </li>
+            <li style={{ '--i': 5 , backgroundImage: 'linear-gradient(green 0%, #34335D 52%, #34335D 92%, green 100%)'}}>
+              <a href="#"><Bluetooth_Icon /></a>
+            </li>
+            <li style={{ '--i': 6 , backgroundImage: 'linear-gradient(blue 0%, #34335D 52%, #34335D 92%, blue 100%)'}}>
+              <a href="#"><Ai_Assist_Icon /></a>
+            </li>
+            <li style={{ '--i': 7 , backgroundImage: 'linear-gradient(red 0%, #34335D 52%, #34335D 92%, red 100%)'}}>
+              <a href="#"><Trip_Comp_Icon /></a>
+            </li>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
