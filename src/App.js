1d219ac95d19d35fc8495db0bdf6e27eb3fee9a9
john calentine <112145061+calentine@users.noreply.github.com>
2024-08-08 18:06:03 -0400

    added buttons for testing purposes

--- src/App.js
+++ src/App.js
@@ -29,28 +29,28 @@ function App() {
             <Wheel_Icon />
             </div>
             <li style={{ '--i': 0, backgroundImage: 'linear-gradient(orange 0%, #34335D 52%, #34335D 92%, orange 100%)' }}>
-              <a href="#"><Nav_Icon /></a>
+              <button className='button_wrapper' onClick={() => console.log("Navigation Pressed")} ><Nav_Icon /></button>
             </li>
             <li style={{ '--i': 1, backgroundImage: 'linear-gradient(green 0%, #34335D 52%, #34335D 92%, green 100%)' }}>
-              <a href="#"><Phone_Icon/></a>
+            <button className='button_wrapper' onClick={() => console.log("Phone Pressed")} ><Phone_Icon /></button>
             </li>
             <li style={{ '--i': 2 , backgroundImage: 'linear-gradient(blue 0%, #34335D 52%, #34335D 92%, blue 100%)'}}>
-              <a href="#"><Android_Icon /></a>
+            <button className='button_wrapper' onClick={() => console.log("Android Pressed")} ><Android_Icon /></button>
             </li>
             <li style={{ '--i': 3, backgroundImage: 'linear-gradient(red 0%, #34335D 52%, #34335D 92%, red 100%)' }}>
-              <a href="#"><FM_Icon /></a>
+            <button className='button_wrapper' onClick={() => console.log("FM Pressed")} ><FM_Icon /></button>
             </li>
             <li style={{ '--i': 4 , backgroundImage: 'linear-gradient(orange 0%, #34335D 8%, #34335D 52%, orange 100%)'}}>
-              <a href="#"><Apps_Icon /></a>
+            <button className='button_wrapper' onClick={() => console.log("Apps Pressed")} ><Apps_Icon /></button>
             </li>
             <li style={{ '--i': 5 , backgroundImage: 'linear-gradient(green 0%, #34335D 52%, #34335D 92%, green 100%)'}}>
-              <a href="#"><Bluetooth_Icon /></a>
+            <button className='button_wrapper' onClick={() => console.log("Bluetooth Pressed")} ><Bluetooth_Icon /></button>
             </li>
             <li style={{ '--i': 6 , backgroundImage: 'linear-gradient(blue 0%, #34335D 52%, #34335D 92%, blue 100%)'}}>
-              <a href="#"><Ai_Assist_Icon /></a>
+            <button className='button_wrapper' onClick={() => console.log("Ai Assist Pressed")} ><Ai_Assist_Icon /></button>
             </li>
             <li style={{ '--i': 7 , backgroundImage: 'linear-gradient(red 0%, #34335D 52%, #34335D 92%, red 100%)'}}>
-              <a href="#"><Trip_Comp_Icon /></a>
+            <button className='button_wrapper' onClick={() => console.log("Trip Info Pressed")} ><Trip_Comp_Icon /></button>
             </li>
           </div>
         </div>
